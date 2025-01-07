7850a7b90589634614b3c1d895bb0d75bd8a4777
Antonio <antoniogabriel2002@gmail.com>
2025-01-07 10:21:57 -0300

    Display a localised greeting to user

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// // along with Moodle. If not, see <https://www.gnu.org/licenses/>.
+// along with Moodle. If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * @package     local_greetings
@@ -21,18 +21,25 @@
  */
 
 require_once('../../config.php'); // Inclui o arquivo de configuração do Moodle.
+require_once($CFG->dirroot. '/local/greetings/lib.php'); // Inclui o arquivo lib.php.
+
 $context = context_system::instance();// Obtém o contexto do sistema.
 $PAGE->set_context($context);// Define o contexto da página.
 $PAGE->set_url('/local/greetings/index.php'); // Define a URL da página.
 $PAGE->set_pagelayout('standard');// Define o layout da página.
 $PAGE->set_title(get_string('pluginname' , 'local_greetings'));
 $PAGE->set_heading(get_string('pluginname' , 'local_greetings'));
+
 echo $OUTPUT->header(); // Imprime o cabeçalho da página.
+
 // echo '<h3>Saudações, meu Rei</h3>'; // Imprime uma mensagem de saudação.
 if (isloggedin()) {
-    echo '<h3>Boas vindas meu rei, ' . fullname($USER) . '</h3>'; // Imprime uma mensagem de saudação personalizada.
+    // echo '<h3>Boas vindas meu rei, ' . fullname($USER) . '</h3>'; // Imprime uma mensagem de saudação personalizada.
+    // echo get_string('greetingloggedinuser', 'local_greetings', fullname($USER));
+    echo local_greetings_get_greeting($USER);
 } else {
-    echo '<h3>Boas vidas jovem! user</h3>';
+    // echo '<h3>Boas vidas jovem! user</h3>';
+    echo get_string('greetinguser', 'local_greetings');
 }
 
 echo $OUTPUT->footer(); // Imprime o rodapé da página.
